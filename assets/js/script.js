fa0bff00121444f69ecd6151a6eaf213128ba3cd
geniusgateid <geniusgateindonesia@gmail.com>
2024-10-27 03:43:26 +0800

    add filesss

---
+++ assets/js/script.js
@@ -0,0 +1,4 @@
+var scene = document.getElementById('scene');
+var parallaxInstance = new Parallax(scene);
+var scene2 = document.getElementById('scene2');
+var parallaxInstance = new Parallax(scene2);
